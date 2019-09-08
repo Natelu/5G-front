ecc6370f94467d61e47021a659c3c3d55b6454da
luzx23 <luzx23@chinaunicom.cn>
2019-09-08 11:35:38 +0800

    change the remote server to ourselves

--- 5G-Base-Front/config/index.js
+++ 5G-Base-Front/config/index.js
@@ -14,8 +14,8 @@ module.exports = {
     // 代理列表, 是否开启代理通过[./dev.env.js]配置
     proxyTable: devEnv.OPEN_PROXY === false ? {} : {
       '/proxyApi': {
-        target: 'http://120.79.81.9:8080/city-fire/',
-        // target:'http://100.6.10.176:8080/city-fire/',
+        // target: 'http://120.79.81.9:8080/city-fire/',
+        target:'http://100.4.8.64:8080/city-fire/',
         changeOrigin: true,
         pathRewrite: {
           '^/proxyApi': '/'
